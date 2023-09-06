2169abb50fb139ddd46991da71ca6a02b8dd545d
MarwaAbuEssa <meromeroo@gmail.com>
2023-09-05 18:26:08 -0700

    last digit fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_digit = (abs(number) % 10) * -1
+    last_digit = abs(number) % 10
+    last_digit = -last_digit
 else:
     last_digit = number % 10
 if last_digit > 5:
