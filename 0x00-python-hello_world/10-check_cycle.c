e82b2c2f24e729da560b2fbd43785e17be4f4ace
MarwaAbuEssa <meromeroo@gmail.com>
2023-09-06 17:16:43 -0700

    check cycle in linkedlist

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,21 +9,22 @@
 
 int check_cycle(listint_t *list)
 {
+	listint_t *node1, *node2;
+
 	if (list == NULL || list->next == NULL)
-	{
 		return (0);
-	}
 
-	listint_t *node1 = list;
-	listint_t *node2 = list;
+	node1 = list->next;
+	node2 = list->next->next;
 
-	while (node2 != NULL && node2->next != NULL)
+	while (node1 && node2 && node2->next)
 	{
+		if (node1 == node2)
+			return (1);
+
 		node1 = node1->next;
 		node2 = node2->next->next;
-		if (node1 == node2)
-		{
-			return (1);
-		}
 	}
+
+	return (0);
 }
