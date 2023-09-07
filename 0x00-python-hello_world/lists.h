d2cdc8799488cbeb3279c88615d23542d12de9a7
MarwaAbuEssa <meromeroo@gmail.com>
2023-09-06 17:23:40 -0700

    fix inheader

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,12 +2,17 @@
 #define LISTS_H
 
 #include <stdlib.h>
-
+/**
+ * struct listint_s - linkedlist
+ * @n: integer
+ * @next: pointer
+ * Description: singly linked list
+ */
 
 typedef struct listint_s
 {
-		int n;
-			struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
