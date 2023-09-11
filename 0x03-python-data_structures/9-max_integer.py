7c221b015a5ce71a38c8b0e7e26a2f9bdf5ac4d5
MarwaAbuEssa <meromeroo@gmail.com>
2023-09-11 07:11:45 -0700

    fixing in max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,7 +6,6 @@ def max_integer(my_list=[]):
     max_num = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > max_num:
-            big = my_list[i]
+            max_num = my_list[i]
 
     return (max_num)
-
