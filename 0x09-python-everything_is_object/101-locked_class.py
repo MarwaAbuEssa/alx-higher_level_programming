1cb3eb5f6caaaafa8ca424e0125eac853b1bbc0f
MarwaAbuEssa <meromeroo@gmail.com>
2023-10-04 04:05:24 -0700

    locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+class LockedClass:
+    """
+    the user can't init LockedClass attributes
+    for all except 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
