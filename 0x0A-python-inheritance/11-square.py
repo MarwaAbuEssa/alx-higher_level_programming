8584fc2d1c0156da4b6e49149865a65f295b1722
MarwaAbuEssa <meromeroo@gmail.com>
2023-10-09 18:56:28 -0700

    add str

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -12,6 +12,15 @@ def __init__(self, size):
     Args:
         size (int): the size of new square
     """
+    super().__init__(self.__size, self.__size)
     self.integer_validator("size", size)
-    super().__init__(size, size)
     self.__size = size
+
+def area(self):
+    """area"""
+
+    return self.__size * self.__size
+
+
+def __str__(self):
+    return "[Square] {}/{}".format(self.__size, self.__size)
