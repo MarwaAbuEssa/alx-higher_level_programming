f4f7b42a43deb95a34d733b955b5d63ab2c9d5b1
MarwaAbuEssa <meromeroo@gmail.com>
2023-10-10 16:07:34 -0700

    student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" class Student."""
+
+
+class Student:
+    """  a student."""
+
+
+    def __init__(self, first_name, last_name, age):
+        """ init new student
+
+        Args:
+            first_name (str): The first name
+            last_name (str): The last name
+            age (int): The age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ dic of the Student."""
+        return self.__dict__
