eaa1192d5efe858f38e887186f5a9fd31a1ce554
MarwaAbuEssa <meromeroo@gmail.com>
2023-10-10 15:48:56 -0700

    save json in txt file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" JSON file write  """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ object to a text file using JSON """
+
+    with open(filename, "w") as myFile:
+        json.dump(my_obj, myFile)
