fb6757072ce29e1c937031cbb9fe76c67c9fbd13
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-17 15:42:14 -0800

    1 filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,6 +3,9 @@
 # Usage: ./1-filter_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
+import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
