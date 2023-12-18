655f1a1b40d708b8c3bbbaa30a3dbe915b614dbc
Marwa Mohamed <meromeroo@gmail.com>
2023-12-19 00:58:52 +0200

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# get first State hbtn_0e_6_usa.
-# Usage: ./8-model_state_fetch_first.py <mysql username> /
-#                                       <mysql password> /
-#                                       <database name>
+""" get first State hbtn_0e_6_usa.
+Usage: ./8-model_state_fetch_first.py <mysql username> \
+                                      <mysql password> \
+                                      <database name>
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
+
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
