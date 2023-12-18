5c616b4f6ba4e7d642062978fcabbd801686877a
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-18 06:16:15 -0800

    10- model state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """  State by name passed
-Usage: ./10-model_state_my_get.py <mysql username> /
-                                   <mysql password> /
-                                    <database name>
-                                   <state name searched>
+Usage: ./10-model_state_my_get.py   <mysql username> \
+                                    <mysql password> \
+                                    <database name> \
+                                    <state name searched>
 """
 import sys
 from sqlalchemy import create_engine
