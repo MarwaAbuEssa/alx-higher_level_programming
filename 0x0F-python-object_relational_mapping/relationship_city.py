1a5f378148b4feec084ee6c006a3cf7526681f22
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-18 15:08:25 -0800

    relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,9 +11,9 @@ class City(Base):
     """ a city for a MySQL.
 
     Attributes:
-        id (sqlalchemy.Column): The city's id.
-        name (sqlalchemy.Column): The city's name.
-        state_id (sqlalchemy.Column): The city's state id.
+    id (sqlalchemy.Column): The city's id.
+    name (sqlalchemy.Column): The city's name.
+    state_id (sqlalchemy.Column): The city's state id.
     """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
