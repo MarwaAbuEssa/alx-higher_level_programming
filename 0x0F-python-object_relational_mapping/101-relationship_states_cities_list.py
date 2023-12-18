4e1f2ff86c7027f26d3ba85472898dac1c06ed3f
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-18 15:27:18 -0800

    fix unix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-    """ all States and Cities in  hbtn_0e_101_usa.
+""" all States and Cities in  hbtn_0e_101_usa.
     Usage: ./101-relationship_states_cities_list.py <mysql username>
                                                     <mysql password>
                                                     <database name>
-    """
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
