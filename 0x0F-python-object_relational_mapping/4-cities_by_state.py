1240618b2398b838d774f8b27eb9c5ec518b2201
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-18 01:36:13 -0800

    cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ all cities hbtn_0e_4_usa, ordered by city id.
-Usage: ./4-cities_by_state.py <mysql username> \
-                                <mysql password> \
-                               <database name>
+Usage: ./4-cities_by_state.py   <mysql username>\
+                                <mysql password>\
+                                <database name>
 """
 import sys
 import MySQLdb
