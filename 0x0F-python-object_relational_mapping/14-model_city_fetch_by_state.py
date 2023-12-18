45c1006661ef453555d6dfbfa00aa058e337da53
Marwa Mohamed <meromeroo@gmail.com>
2023-12-18 16:36:27 +0200

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-# Lists City
-# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
-#                                          <mysql password> /
-#                                          <database name>
+""" Lists City
+Usage: ./14-model_city_fetch_by_state.py <mysql username> \
+                                          <mysql password> \
+                                          <database name>
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 from model_city import City
+
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
