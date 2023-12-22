97b7fdc83bb876d79cb1ea062e638bcfd7f57323
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-22 00:54:58 -0800

    model city fix style

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class City(Base):
     """ a city for a MySQL.
 
@@ -14,7 +15,7 @@ class City(Base):
         name (sqlalchemy.Integer): The city's name.
         state_id (sqlalchemy.String): The city's state id.
     """
-    
+
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
