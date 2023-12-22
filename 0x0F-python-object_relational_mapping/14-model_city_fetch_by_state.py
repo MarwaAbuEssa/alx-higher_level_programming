74fbfc3597d9b1419423b3ae97c8a1028478e565
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-22 00:52:17 -0800

    14 model city

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Lists City
-Usage: ./14-model_city_fetch_by_state.py <mysql username> \
-                                          <mysql password> \
-                                          <database name>
+Usage: ./14-model_city_fetch_by_state.py <mysql username>
+                                         <mysql password>
+                                         <database name>
 """
 import sys
 from sqlalchemy import create_engine
