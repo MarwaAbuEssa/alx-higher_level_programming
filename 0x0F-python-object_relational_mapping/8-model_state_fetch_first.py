a939dd762632665a9edd4a32a3c614a50bb71f20
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-22 00:22:36 -0800

    model state fetch first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ get first State hbtn_0e_6_usa.
-Usage: ./8-model_state_fetch_first.py <mysql username> \
-                                      <mysql password> \
+Usage: ./8-model_state_fetch_first.py <mysql username>
+                                      <mysql password>
                                       <database name>
 """
 import sys
