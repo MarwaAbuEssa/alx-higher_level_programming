d08337d507c35bdbd3fdfae744fc87be4feb0458
MarwaAbuEssa <meromeroo@gmail.com>
2024-01-06 16:41:33 -0800

    fix in 0 status and 1 header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 if __name__ == "__main__":
     request = urllib.request.Request("https://intranet.hbtn.io/status")
-    ith urllib.request.urlopen(request) as response:
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
