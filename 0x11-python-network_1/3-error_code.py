4ce9af7d5eb0abab19f626678e14ce78d626837d
MarwaAbuEssa <meromeroo@gmail.com>
2024-01-06 17:05:10 -0800

    htn header

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     request = urllib.request.Request(url)
-        try:
-            with urllib.request.urlopen(request) as response:
-                print(response.read().decode("ascii"))
-        except urllib.error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
